9b9e9bafafdc4b89b7a1e6917567905b5fff56bc
Pedro Henrique <preisteixeira@gmail.com>
2024-07-02 16:15:26 -0400

    add Alerts e logout
    Add alerts na view e nos html, add view para logout, add url para logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from usuarios.forms import LoginForms,CadastroForms
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth,messages
+
 
 # Create your views here.
 def login(request):
@@ -21,8 +22,10 @@ def login(request):
         )
         if usuario is not None:
             auth.login(request,usuario)
+            messages.success(request,'Usuário {nome} logado com sucesso!')
             return redirect('index')
         else:
+            messages.error(request,'Erro ao efetuar login')
             return redirect('login')
         
 
@@ -37,6 +40,7 @@ def cadastro(request):
         
         if form.is_valid():
             if form['senha_1'].value() != form['senha_2'].value():
+                messages.error(request,'As senhas não são iguais')
                 return redirect('cadastro')
             
             nome=form['nome_cadastro'].value()
@@ -44,6 +48,7 @@ def cadastro(request):
             senha=form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request,'Usuário já existente')
                 return redirect('cadastro')
             
             usuario = User.objects.create_user(
@@ -53,6 +58,12 @@ def cadastro(request):
             )
 
             usuario.save()
+            messages.success(request,'Cadastro efetuado com sucesso!')
             return redirect('login')
 
     return render(request,'usuarios/cadastro.html',{'form':form})
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request,'Logout efetuado com sucesso')
+    return redirect('login')
